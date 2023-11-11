a8c9dac071210400b3ec7e8146acc7b867ded948
Felamar <114794450+Felamar@users.noreply.github.com>
2023-11-11 10:58:48 -0600

    Refactor matrix multiplication check in checker.py

--- checker.py
+++ checker.py
@@ -7,6 +7,3 @@ matrix_C = np.loadtxt('result.txt')
 # Check if the result matrix given by the fox algorithm is correct
 # by comparing it with the matrix multiplication using numpy
 print((matrix_A @ matrix_B == matrix_C).all())
-
-print(matrix_C)
-print(matrix_A @ matrix_B)
