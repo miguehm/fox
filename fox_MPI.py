d21a4dd071962a662a5f18d9a311881cbaadad2f
Felamar <114794450+Felamar@users.noreply.github.com>
2023-11-11 15:28:11 -0600

    Refactor fox_MPI.py to accept command line arguments and improve code performance.

--- fox_MPI.py
+++ fox_MPI.py
@@ -1,52 +1,58 @@
+import sys
 import numpy as np
-from mpi4py import MPI
+
 from time import time
-from tqdm.auto import tqdm
+from mpi4py import MPI
 
-def fox_MPI(exponent: int, isInt: bool) -> float:
-    comm = MPI.COMM_WORLD  # get the communicator object
-    size = comm.Get_size() # total number of processes
-    rank = comm.Get_rank() # rank of this process
+exponent = int(sys.argv[1])
+isInt    = bool(sys.argv[2])
 
-    if rank == 0: # if the process is the master 
-        MATRIX_SIZE = 2**exponent
-        # generate two random matrix of size MATRIX_SIZE
-        if isInt:
-            matrix_A    = np.random.randint(1000, 2000, (MATRIX_SIZE, MATRIX_SIZE)) 
-            matrix_B    = np.random.randint(1000, 2000, (MATRIX_SIZE, MATRIX_SIZE)) 
-            # initialize the matrix C with zeros
-            matrix_C    = np.zeros((MATRIX_SIZE, MATRIX_SIZE), dtype=int)
-        else:
-            matrix_A    = np.random.uniform(1000, 2000, (MATRIX_SIZE, MATRIX_SIZE))
-            matrix_B    = np.random.uniform(1000, 2000, (MATRIX_SIZE, MATRIX_SIZE))
-            # initialize the matrix C with zeros
-            matrix_C    = np.zeros((MATRIX_SIZE, MATRIX_SIZE))
-        
-        data = (MATRIX_SIZE, matrix_A, matrix_B, matrix_C)
+comm = MPI.COMM_WORLD  # get the communicator object
+size = comm.Get_size() # total number of processes
+rank = comm.Get_rank() # rank of this process
 
+if rank == 0: # if the process is the master 
+    MATRIX_SIZE = 2**exponent
+    # generate two random matrix of size MATRIX_SIZE
+    if isInt:
+        matrix_A    = np.random.randint(1000, 2000, (MATRIX_SIZE, MATRIX_SIZE)) 
+        matrix_B    = np.random.randint(1000, 2000, (MATRIX_SIZE, MATRIX_SIZE)) 
+        # initialize the matrix C with zeros
+        matrix_C    = np.zeros((MATRIX_SIZE, MATRIX_SIZE), dtype=int)
     else:
-        data = None
+        matrix_A    = np.random.uniform(1000, 2000, (MATRIX_SIZE, MATRIX_SIZE))
+        matrix_B    = np.random.uniform(1000, 2000, (MATRIX_SIZE, MATRIX_SIZE))
+        # initialize the matrix C with zeros
+        matrix_C    = np.zeros((MATRIX_SIZE, MATRIX_SIZE))
+    
+    data = (MATRIX_SIZE, matrix_A, matrix_B, matrix_C)
 
-    data = comm.bcast(data, root=0)                  # broadcast the data to all processes
-    MATRIX_SIZE, matrix_A, matrix_B, matrix_C = data # unpack the data
-    start_time = time()
+else:
+    data = None
 
-    for row_i in tqdm(range(MATRIX_SIZE)): 
-        # Each process calculates a row of the matrix C
-        # The process with rank i calculates the row i of the matrix C
-        # The row i of the matrix C is the sum of the rows of the matrix A multiplied by the matrix B
+data = comm.bcast(data, root=0)                  # broadcast the data to all processes
+MATRIX_SIZE, matrix_A, matrix_B, matrix_C = data # unpack the data
+start_time = time()
 
-        # this ensures that each process calculates a row of the matrix C without repeating rows
-        if rank == row_i % size and rank < MATRIX_SIZE: 
-            for i in range(MATRIX_SIZE):
-                # a[row_i, row_i] gets shifted to the right by i positions
-                # and b[row_i] gets shifted to the bottom by i positions
-                col = (row_i + i) % MATRIX_SIZE
-                matrix_C[row_i] += matrix_A[row_i, col] * matrix_B[col] 
+for row_i in range(MATRIX_SIZE): 
+    # Each process calculates a row of the matrix C
+    # The process with rank i calculates the row i of the matrix C
+    # The row i of the matrix C is the sum of the rows of the matrix A multiplied by the matrix B
 
-        # The rows of the matrix C are distributed among the processes using the MPI_Allreduce function
-        # The MPI_Allreduce function sums the rows of the matrix C calculated by each process
-        comm.Allreduce(MPI.IN_PLACE, matrix_C[row_i], op=MPI.SUM)
+    # this ensures that each process calculates a row of the matrix C without repeating rows
+    if rank == row_i % size: 
+        for i in range(MATRIX_SIZE):
+            # a[row_i, row_i] gets shifted to the right by i positions
+            # and b[row_i] gets shifted to the bottom by i positions
+            col = (row_i + i) % MATRIX_SIZE
+            matrix_C[row_i] += matrix_A[row_i, col] * matrix_B[col] 
 
-    if rank == 0:
-        return time() - start_time
+# The rows of the matrix C are distributed among the processes using the MPI_Allreduce function
+# The MPI_Allreduce function sums the rows of the matrix C calculated by each process
+comm.Allreduce(MPI.IN_PLACE, matrix_C, op=MPI.SUM)
+
+if rank == 0:
+    print(time() - start_time)
+    np.savetxt('arrayA.txt', matrix_A, fmt='%d')
+    np.savetxt('arrayB.txt', matrix_B, fmt='%d')
+    np.savetxt('result.txt', matrix_C, fmt='%d')
