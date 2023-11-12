307d168c7ab8dd0a5717306f531751663a563651
Felamar <114794450+Felamar@users.noreply.github.com>
2023-11-12 14:42:38 -0600

    Add text argument to subprocess.run() calls and include memory usage in graphs

--- src/main.py
+++ src/main.py
@@ -4,16 +4,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def run_mpi_file(mpi_file_path, exponent, isInt=True):
-    result = subprocess.run(['mpiexec', '-n', '8', 'python', mpi_file_path, f'{exponent}', f'{isInt}'], stdout=subprocess.PIPE)
-    return result.stdout.decode('utf-8')
+    result = subprocess.run(['mpiexec', '-n', '8', 'python', mpi_file_path, f'{exponent}', f'{isInt}'], stdout=subprocess.PIPE, text=True)
+    #return result.stdout.decode('utf-8')
+    return result.stdout.splitlines()
 
 def run_sec_file(sec_file_path, exponent, isInt=True):
-    result = subprocess.run(['python', sec_file_path, f'{exponent}', f'{isInt}'], stdout=subprocess.PIPE)  
-    return result.stdout.decode('utf-8')  
+    result = subprocess.run(['python', sec_file_path, f'{exponent}', f'{isInt}'], stdout=subprocess.PIPE, text=True)  
+    #return result.stdout.decode('utf-8')  
+    return result.stdout.splitlines()
 
 def data(min_e, max_e, isInt=True):
     times_MPI = {}
     times_SEC = {}
+    memory_MPI = {}
+    memory_SEC = {}
     np.random.seed(69)  # set the seed for the random numbers generator
                         # so that the same numbers are generated in each process
     for exponent in range(min_e, max_e):
@@ -21,10 +25,10 @@ def data(min_e, max_e, isInt=True):
         times_SEC[exponent] = np.zeros(5)
         print(f'Calculating for {2**exponent}...')
         for i in range(5):
-            times_SEC[exponent][i] = run_sec_file('src/fox_SEC.py', exponent, isInt=isInt)
-            times_MPI[exponent][i] = run_mpi_file('src/fox_MPI.py', exponent, isInt=isInt)
+            times_SEC[exponent][i], memory_MPI[exponent][i] = run_sec_file('src/fox_SEC.py', exponent, isInt=isInt)
+            times_MPI[exponent][i], memory_SEC[exponent][i] = run_mpi_file('src/fox_MPI.py', exponent, isInt=isInt)
     
-    return times_MPI, times_SEC
+    return times_MPI, times_SEC, memory_MPI, memory_SEC
 
 def create_dir(path, e_range, numtype):
     if not os.path.exists(path):
@@ -35,42 +39,65 @@ def create_dir(path, e_range, numtype):
 
 def graphs(min_e, max_e, isInt=True):
 
-    times_MPI, times_SEC = data(min_e, max_e, isInt=isInt)
+    times_MPI, times_SEC, memory_MPI, memory_SEC = data(min_e, max_e, isInt=isInt)
 
-    y_MPI = [np.mean(times_MPI[exponent]) for exponent in times_MPI]
-    y_SEC = [np.mean(times_SEC[exponent]) for exponent in times_SEC]
+    y_Time_MPI   = [np.mean(times_MPI[exponent])  for exponent in times_MPI ]
+    y_Time_SEC   = [np.mean(times_SEC[exponent])  for exponent in times_SEC ]
+    y_Memory_MPI = [np.mean(memory_MPI[exponent]) for exponent in memory_MPI]
+    y_Memory_SEC = [np.mean(memory_SEC[exponent]) for exponent in memory_SEC]
     
     bar_w = 0.35
-    x_MPI = np.array([exponent - bar_w/2 for exponent in times_MPI])
-    x_SEC = np.array([exponent + bar_w/2 for exponent in times_SEC])
+    x_Time_MPI   = np.array([exponent - bar_w/2 for exponent in times_MPI ])
+    x_Time_SEC   = np.array([exponent + bar_w/2 for exponent in times_SEC ])
+    x_Memory_MPI = np.array([exponent - bar_w/2 for exponent in memory_MPI])
+    x_Memory_SEC = np.array([exponent + bar_w/2 for exponent in memory_SEC])
 
-    x_labels = [str(2**exponent) for exponent in times_MPI]
+    x_Labels = [str(2**exponent) for exponent in times_MPI]
     num_type = 'enteros' if isInt else 'reales'
 
-    fig, (T_ax, M_ax) = plt.subplots(1, 2, figsize=(20, 5))
+    fig, (Time_ax, Memory_ax) = plt.subplots(1, 2, figsize=(20, 5))
     plt.subplots_adjust(left=0.1, right=0.9, bottom=0.1, top=0.9, wspace=0.5, hspace=0.5)
+    fig.suptitle(f'Comparación de las ejecuciones del algoritmo Fox con matrices de números {num_type}.')
     
-    T_ax.set_title(f'Tiempo de ejecución del algoritmo fox con\nmatrices de números {num_type}.')
-    T_ax.set_xlabel('Orden de la Matriz')
-    T_ax.set_ylabel('Tiempo de ejecución (s)')
-    T_ax.set_xticks(x_MPI + bar_w/2, x_labels)
-    T_ax.set_yticks([]) 
+    Time_ax.set_title(f'Gráfica comparativa de los tiempos de ejecución entre ejecuciones\nsecuenciales y paralelas.')
+    Time_ax.set_xlabel('Orden de la Matriz')
+    Time_ax.set_ylabel('Tiempo de ejecución (s)')
+    Time_ax.set_xticks(x_Time_MPI + bar_w/2, x_Labels)
+    Time_ax.set_yticks([]) 
 
-    bars_MPI = T_ax.bar(x_MPI, y_MPI, bar_w, label='MPI',        color='#EA75FA')
-    bars_SEC = T_ax.bar(x_SEC, y_SEC, bar_w, label='Secuencial', color='#4590FA')
-    
-    for bar in bars_SEC + bars_MPI:
+    bars_Time_MPI = Time_ax.bar(x_Time_MPI, y_Time_MPI, bar_w, label='MPI',        color='#EA75FA')
+    bars_Time_SEC = Time_ax.bar(x_Time_SEC, y_Time_SEC, bar_w, label='Secuencial', color='#4590FA')
+    for bar in bars_Time_SEC + bars_Time_MPI :
         x_pos   = bar.get_x() + bar.get_width()/2.0
         y_value = bar.get_height()
-        T_ax.text(
+        Time_ax.text(
             x_pos, 
             y_value, 
             f'{y_value:5.5}s', 
             va='bottom', 
             ha='center'
         )
-    
-    T_ax.legend()
+    Time_ax.legend()
+
+    Memory_ax.set_title(f'Grafica comparativa de la memoria (RAM) utilizada entre ejecuciones\nsecuenciales y paralelas.')
+    Memory_ax.set_xlabel('Orden de la Matriz')
+    Memory_ax.set_ylabel('Memoria utilizada (MB)')
+    Memory_ax.set_xticks(x_Memory_MPI + bar_w/2, x_Labels)
+    Memory_ax.set_yticks([])
+
+    bars_Memory_MPI = Memory_ax.bar(x_Memory_MPI, y_Memory_MPI, bar_w, label='MPI',        color='#EA75FA')
+    bars_Memory_SEC = Memory_ax.bar(x_Memory_SEC, y_Memory_SEC, bar_w, label='Secuencial', color='#4590FA')
+    for bar in bars_Memory_SEC + bars_Memory_MPI :
+        x_pos   = bar.get_x() + bar.get_width()/2.0
+        y_value = bar.get_height()
+        Memory_ax.text(
+            x_pos, 
+            y_value, 
+            f'{(y_value/1024):5.5}MB', 
+            va='bottom', 
+            ha='center'
+        )
+    Memory_ax.legend()
     
     fig.savefig(create_dir('graphs', f'{2**(min_e)}-{2**(max_e-1)}', num_type))
 
