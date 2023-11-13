6c19d7b62e2bbd519cbb6e5d6810dad5210a1c4d
Felamar <114794450+Felamar@users.noreply.github.com>
2023-11-12 19:27:28 -0600

    Update file paths for matrix loading

--- src/checker.py
+++ src/checker.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-matrix_A = np.loadtxt('arrayA.txt')
-matrix_B = np.loadtxt('arrayB.txt')
-matrix_C = np.loadtxt('result.txt')
+matrix_A = np.loadtxt('/results/matrix_A.data')
+matrix_B = np.loadtxt('/results/matrix_B.data')
+matrix_C = np.loadtxt('/results/matrix_C.data')
 
 # Check if the result matrix given by the fox algorithm is correct
 # by comparing it with the matrix multiplication using numpy
